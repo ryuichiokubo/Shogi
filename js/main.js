62ddfd2486b96013005aac5a4ec495a1221737e3
Ryuichi Okubo <ryuichi.okubo.1986@gmail.com>
2014-04-18 13:07:16 +0300

    When empty place on board is clicked, calculate its position class name

--- js/main.js
+++ js/main.js
@@ -4,14 +4,46 @@
 var main = function () {
 
     var i = 0;
-    var set = document.querySelector(".set");    
     var classAttr = '';
     var srcPath = '';
     var p;
+    var selected;
+    
+    var getPosClass = function(row, column) {
+        var rowNames = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+        var columnNames = ['ichi', 'ni', 'san', 'yon', 'go', 'roku', 'nana', 'hachi', 'kyu'];
+        var res = rowNames[row-1] + ' ' + columnNames[column-1];
+        
+        return res;
+    };
+    
+    var set = document.querySelector(".set");
+    set.addEventListener('click', function(event) {
+        var x, y, row, column, posClass;
+        console.log(event);
+
+        if (event.target.className === 'board') {
+            x = event.offsetX;
+            y = event.offsetY;
+            row = Math.ceil(x / 60);
+            column = Math.ceil(y / 60);
+            posClass = getPosClass(row, column);
+
+            if (selected) {
+                selected.setAttribute('class', 'piece ' + posClass);
+            }
+        }
+
+    });
+
+    
+    var pieceSelect = function(event) {
+        selected = event.target;
+        event.target.style['background-color'] = 'yellow';
+    };
+
     for (i = 0; i < initPieces.length; i++) {
-        
-        console.log(initPieces[i]);
-        
+    
         p = document.createElement('img');
         
         srcPath = 'svg/' +  initPieces[i].piece + '.svg';
@@ -23,6 +55,8 @@ var main = function () {
         }
         p.setAttribute('class', classAttr);
 
+        p.addEventListener('click', pieceSelect);
+        
         set.appendChild(p);
     }
 
