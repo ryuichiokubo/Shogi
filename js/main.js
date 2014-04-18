caf4cf56b00cbcc838e46015173290a18e3865a6
Ryuichi Okubo <ryuichi.okubo.1986@gmail.com>
2014-04-18 14:17:44 +0300

    Piece can move to where other piece is

--- js/main.js
+++ js/main.js
@@ -3,45 +3,76 @@
 
 var main = function () {
 
-    var i = 0;
+    var i;
     var classAttr = '';
     var srcPath = '';
     var p;
     var selected;
     
+    var rowNames = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    var columnNames = ['ichi', 'ni', 'san', 'yon', 'go', 'roku', 'nana', 'hachi', 'kyu'];
+
     var getPosClass = function(row, column) {
-        var rowNames = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
-        var columnNames = ['ichi', 'ni', 'san', 'yon', 'go', 'roku', 'nana', 'hachi', 'kyu'];
         var res = rowNames[row-1] + ' ' + columnNames[column-1];
         
         return res;
     };
     
+    var pieceSelect = function(event) {
+        if (event.target.className.indexOf('oppoPiece') > -1) {
+            return;
+        }
+
+        if (selected) {
+            selected.style["background-color"] = '';
+        }
+        selected = event.target;
+        event.target.style['background-color'] = 'yellow';
+    };
+
     var set = document.querySelector(".set");
     set.addEventListener('click', function(event) {
-        var x, y, row, column, posClass;
-        console.log(event);
+        var x, y, row, column, posClass, classes, rowHit, columnHit;
 
         if (event.target.className === 'board') {
+            // no piece where it is clicked
             x = event.offsetX;
             y = event.offsetY;
             row = Math.ceil(x / 60);
             column = Math.ceil(y / 60);
             posClass = getPosClass(row, column);
-
-            if (selected) {
-                selected.setAttribute('class', 'piece ' + posClass);
+        } else {
+            // piece is clicked
+            
+            if (event.target.className.indexOf('oppoPiece') > -1) {
+                // opponent's piece
+                classes = event.target.className.split(' ');
+                for (i = 0; i < classes.length; i++) {
+                    if (rowNames.indexOf(classes[i]) > -1) {
+                        rowHit = rowNames[rowNames.indexOf(classes[i])];
+                    }
+                    if (columnNames.indexOf(classes[i]) > -1 ) {
+                        columnHit = columnNames[columnNames.indexOf(classes[i])];
+                    }
+                }
+                posClass = rowHit + ' ' + columnHit;
+                
+                // XXX TODO: move to my mochigoma
+            } else {
+                // my piece
+                pieceSelect(event);
             }
+        }
+
+        if (selected && posClass) {
+            selected.setAttribute('class', 'piece ' + posClass);
+            selected.style["background-color"] = '';
+            selected = null;
         }
 
     });
 
     
-    var pieceSelect = function(event) {
-        selected = event.target;
-        event.target.style['background-color'] = 'yellow';
-    };
-
     for (i = 0; i < initPieces.length; i++) {
     
         p = document.createElement('img');
